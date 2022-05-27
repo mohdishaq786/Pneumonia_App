070c5b79e7ead6accd88b77ef31f310932b899dc
ishaq <49558047+mohdishaq786@users.noreply.github.com>
2022-05-28 02:47:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ ref_lab={0 :'NORMAL', 1 :'PNEUMONIA'}
 model = load_model('model.h5', compile = False)
 
 st.title('Pneumonia Detector')
-st.image('httpsupload.wikimedia.orgwikipediacommons881Chest_radiograph_in_influensa_and_H_influenzae%2C_posteroanterior%2C_annotated.jpg', width = 500)
+st.image('https://upload.wikimedia.org/wikipedia/commons/8/81/Chest_radiograph_in_influensa_and_H_influenzae%2C_posteroanterior%2C_annotated.jpg', width = 500)
 
 
 choice = st.selectbox('Choose one of the following', ('URL', 'Upload Image'))
