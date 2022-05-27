c17759e8d0caddf39f81af21284d4964cd9c6ffd
ishaq <49558047+mohdishaq786@users.noreply.github.com>
2022-05-28 02:30:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img 
+from tensorflow.keras.models import load_model
+import os
+from skimage.io import imread
+from skimage.transform import resize
+
+from tensorflow.keras.optimizers import Adam
+
+from PIL import Image
+
+
+ref_lab={0 :'NORMAL', 1 :'PNEUMONIA'}
+model = load_model('model.h5', compile = False)
+
+st.title('Pneumonia Detector')
+st.image('httpsupload.wikimedia.orgwikipediacommons881Chest_radiograph_in_influensa_and_H_influenzae%2C_posteroanterior%2C_annotated.jpg', width = 500)
+
+
+choice = st.selectbox('Choose one of the following', ('URL', 'Upload Image'))
+try:
+  if choice == 'URL':
+    image_path = st.text_input('Enter image URL...')
+    try:
+      img = imread(image_path)
+      img = resize(img, (256, 256))
+    except:
+      st.markdown('Enter a URL')
+
+  if choice == 'Upload Image':
+    img = st.file_uploader('Upload an Image')
+    try:
+      img = Image.open(img)
+      img = np.array(img)/255
+      img = resize(img, (256, 256))
+    except:
+        st.markdown('Upload a valid image')
+
+ 
+
+  pred = model.predict(np.expand_dims(img, 0))[0]
+  pred=int(pred>0.8)
+  pred= lab.get(pred)
+  
+    
+  text = st.text_area(pred, height=300) 
+
+  st.image(img, caption = 'Input', width = 256)
+  
+except:
+  pass
