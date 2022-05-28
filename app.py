1e375d907513bc178ddaf43af0af614f7985f8b0
ishaq <49558047+mohdishaq786@users.noreply.github.com>
2022-05-28 18:08:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from tensorflow.keras.models import load_model
 model = load_model('model.h5')
   
 st.title('Pneumonia Detector')
+st.image('https://upload.wikimedia.org/wikipedia/commons/8/81/Chest_radiograph_in_influensa_and_H_influenzae%2C_posteroanterior%2C_annotated.jpg', width = 500)
 
 def process(image):
     if image is not None:
@@ -16,11 +17,13 @@ def process(image):
         img = np.array(Image.open(image))
         img = img/255
         img = resize(img, (256, 256))
+        st.image(img, caption = 'Input', width = 256)
         return img
       else:
         img = imread(image)
         img = img/255
         img = resize(img, (256, 256))
+        st.image(img, caption = 'Input', width = 256)
         return img
     else:
         pass #st.text('Upload a Image')	
@@ -29,13 +32,18 @@ def predict(image):
     img = gray2rgb(image)
     img = np.expand_dims(img, axis = 0)   
     pred = model.predict(img)[0]
+    prob = pred
     pred = int(pred>=0.9)
     if pred == 1:
         class_ = 'Pneumonia'
+        st.title('Kindly contact the doctor')
+        
     else:
         class_ = 'Normal'
+        st.title('Well and good')
     
     st.title('Result: ' + class_)
+    st.title('Probality: ' + prob)
 
 choice = st.selectbox('Choose one of the following', ('URL', 'Upload Image'))
 try:
