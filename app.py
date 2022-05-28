2f83fc6b3169640be156a5ea159d0b6ba503f22b
ishaq <49558047+mohdishaq786@users.noreply.github.com>
2022-05-28 17:46:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from skimage.transform import resize
 from skimage.color import gray2rgb
+from skimage.io import imread
 import numpy as np
 import tensorflow as tf
 from PIL import Image
@@ -9,25 +10,42 @@ model = load_model('model.h5')
   
 st.title('Pneumonia Detector')
 
-img_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-
 def process(image):
     if image is not None:
+      if type(image) != str:
         img = np.array(Image.open(image))
         img = img/255
         img = resize(img, (256, 256))
         return img
+      else:
+        img = imread(image)
+        img = img/255
+        img = resize(img, (256, 256))
+        return img
     else:
-        st.text('Upload a Image')	
-if img_file is not None:
-    img = process(img_file)
+        pass #st.text('Upload a Image')	
 
-    st.image(img, clamp = True, channels = 'RGB', caption = 'Input Image')
+choice = st.selectbox('Choose one of the following', ('URL', 'Upload Image'))
+try:
+  if choice == 'URL':
+    image_path = st.text_input('Enter image URL...')
+    try:
+      img = process(image_path)
+    except:
+      st.markdown('Enter a URL')
+
+  if choice == 'Upload Image':
+    img = st.file_uploader('Upload an Image', type=["png", "jpg", "jpeg"])
+    try:
+      img = process(img)
+    except:
+        st.markdown('Upload a valid image')
 
 def predict(image):
     img = gray2rgb(image)
     img = np.expand_dims(img, axis = 0)   
-    pred = model.predict_classes(img)[0]
+    pred = model.predict(img)[0]
+    pred = int(pred>=0.9)
     if pred == 1:
         class_ = 'Pneumonia'
     else:
@@ -35,5 +53,5 @@ def predict(image):
     
     st.title('Result: ' + class_)
 
-if img_file is not None:
+if img is not None:
     predict(img)
