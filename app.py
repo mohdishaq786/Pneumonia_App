a6ab05631a31c77cc584ddf123b573202e3fd944
ishaq <49558047+mohdishaq786@users.noreply.github.com>
2022-05-28 17:36:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,39 @@
 import streamlit as st
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img 
-from tensorflow.keras.models import load_model
-import os
-from skimage.io import imread
 from skimage.transform import resize
 from skimage.color import gray2rgb
+import numpy as np
+import tensorflow as tf
 from PIL import Image
-
-ref_lab={0 :'NORMAL', 1 :'PNEUMONIA'}
-model = load_model('model.h5', compile = False)
-
+from tensorflow.keras.models import load_model 
+model = load_model('model.h5')
+  
 st.title('Pneumonia Detector')
 
-st.image('https://upload.wikimedia.org/wikipedia/commons/8/81/Chest_radiograph_in_influensa_and_H_influenzae%2C_posteroanterior%2C_annotated.jpg', width = 500)
-st.text('Please upload the Chest X-ray')
+img_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
-choice = st.selectbox('Choose one of the following', ('URL', 'Upload Image'))
-try:
-  if choice == 'URL':
-    image_path = st.text_input('Enter image URL...')
-    try:
-      img = imread(image_path)/255
-      img = resize(img, (256, 256))
-      img = gray2rgb(img)
-    except:
-      st.markdown('Enter a URL')
+def process(image):
+    if image is not None:
+        img = np.array(Image.open(image))
+        img = img/255
+        img = resize(img, (256, 256))
+        return img
+    else:
+        st.text('Upload a Image')	
+if img_file is not None:
+    img = process(img_file)
 
-  if choice == 'Upload Image':
-    img = st.file_uploader('Upload an Image')
-    try:
-      img = imread(img)/255
-      p = str(img.shape)
-      st.text_area(p, height=300) 
-      img = resize(img, (256, 256))
-      img = gray2rgb(img)
-      p = str(img.shape)
-      st.text_area(p, height=300)
-      
-    except:
-        st.markdown('Upload a valid image')
+    st.image(img, clamp = True, channels = 'RGB', caption = 'Input Image')
 
-  pred = model.predict(np.expand_dims(img, 0))[0]
-  pred=int(pred>0.8)
-  pred= lab.get(pred)
-  
+def predict(image):
+    img = gray2rgb(image)
+    img = np.expand_dims(img, axis = 0)   
+    pred = model.predict_classes(img)[0]
+    if pred == 1:
+        class_ = 'Pneumonia'
+    else:
+        class_ = 'Normal'
     
-  text = st.text_area(pred, height=300) 
+    st.title('Result: ' + class_)
 
-  st.image(img, caption = 'Input', width = 256)
-  
-except:
-  pass
+if img_file is not None:
+    predict(img)
